5a08cb95896567b9420add27b3ce35514b5daa11
Jt3kt <github.jtekt@gmail.com>
2021-07-14 12:23:08 -0400

    Create Test-LrClusterRemoteAccess.ps1
    Establishes and refreshes PSSessions for a defined array of hosts.

---
+++ src/Private/Test-LrClusterRemoteAccess.ps1
@@ -0,0 +1,47 @@
+using namespace System
+using namespace System.IO
+using namespace System.Collections.Generic
+Function Test-LrClusterRemoteAccess {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $false, Position = 0)]
+        [string[]] $HostNames,
+
+        [string] $UserName = 'logrhythm',
+
+        [string] $KeyFilePath = '~/.ssh/id_rsa'
+    )
+    Begin {
+        # Establish existing PS Sessions
+        $CurrentPSSessions = Get-PSSession
+    }
+    
+    Process {
+        # Cleanup Broken PS Sessions
+        ForEach ($CPSSession in $CurrentPSSessions) {
+            if ($CPSSession.State -like "Broken") {
+                Remove-PSSession -Session $CPSSession
+            }
+        }
+        # Refresh active Ps Sessions
+        $CurrentPSSessions = Get-PSSession
+
+        $Results = [List[object]]::new()
+        ForEach ($hostname in $hostnames) {
+            if ($CurrentPSSessions.ComputerName -notcontains $hostname) {
+                try {
+                    $PSSession = New-PSSession -HostName $hostname -UserName $UserName -KeyFilePath $KeyFilePath
+                    $Results.add($PSSession)
+                } Catch {
+                    $Results.add($PSSession)
+                }
+            } else {
+                $Results.Add($($CurrentPSSessions | Where-Object -Property 'ComputerName' -eq $hostname))
+            }
+
+            Start-Sleep 0.2
+        }
+
+        return $Results
+    }
+}
