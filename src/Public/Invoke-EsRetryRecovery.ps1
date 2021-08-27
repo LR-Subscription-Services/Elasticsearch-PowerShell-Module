0514cdc98df0f3d359c4916f4e8ff370bcf41cfd
Jt3kt <github.jtekt@gmail.com>
2021-08-27 10:31:40 -0400

    Create Invoke-EsRetryRecovery.ps1
    New cmdlet to force reroute retry if an index needs a boost.

---
+++ src/Public/Invoke-EsRetryRecovery.ps1
@@ -0,0 +1,39 @@
+Function Invoke-EsRetryRecovery {
+    [CmdletBinding()]
+    Param(
+
+    )
+
+    Begin {
+        $Headers = [Dictionary[string,string]]::new()
+        $Headers.Add("Content-Type","application/json")
+
+        $Master = Get-EsMaster
+
+        if ($Master.ip) {
+            $BaseUrl = "http://" + $Master.ip + ":9200"
+        } else {
+            $BaseUrl = "http://localhost:9200"
+        }
+
+        $Method = "Post"
+    }
+    
+    Process {
+        $RequestUrl = $BaseUrl + "/_cluster/reroute?retry_failed=true?format=json"
+
+
+        Try {
+            $Response = Invoke-RestMethod $RequestUrl -Method $Method -Headers $Headers 
+        } Catch {
+            if ($_.ErrorDetails.Message) {
+                $ErrorData = $_.ErrorDetails.Message | ConvertFrom-Json
+                Return $ErrorData
+            } else {
+                return $_
+            }
+        }
+        
+        return $Response
+    }
+}
