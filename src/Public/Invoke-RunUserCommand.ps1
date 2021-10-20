84c500ca633ea8bb5a047c5cb8b3614d0af7b1b0
Jt3kt <github.jtekt@gmail.com>
2021-10-20 08:46:00 -0400

    Update Invoke-RunUserCommand.ps1

--- src/Public/Invoke-RunUserCommand.ps1
+++ src/Public/Invoke-RunUserCommand.ps1
@@ -2,36 +2,50 @@ Function Invoke-RunUserCommand {
     [CmdletBinding()]
     Param(
         [Parameter(Mandatory = $true, Position = 0)]
-        [string[]] $Commands,
+        [object] $Stage,
 
         [Parameter(Mandatory = $true, Position = 1)]
-        [string] $Stage,
+        [object] $Nodes,
 
-        [Parameter(Mandatory = $true, Position = 2)]
-        [string] $SSHKeyPath,
-
-        [Parameter(Mandatory = $true, Position = 3)]
-        [object] $Nodes
+        [Parameter(Mandatory = $true, Position = 1)]
+        [string] $SSHKeyPath
     )
     Begin {}
 
     Process {
+        New-ProcessLog -logSev i -logStage $($Stage.Name) -logStep 'Run User Command' -logMessage "Begin Step"
         ForEach ($Node in $Nodes) {
-            if ($Commands) {
-                $NodeSession = Test-LrClusterRemoteAccess -Hostnames $($Node.ipaddr) -Path $SSHKeyPath
-                New-ProcessLog -logSev i -logStage $Stage -logStep 'Run User Command' -logExField2 "Node: $($Node.hostname)" -logMessage "Begin Step"
+            if ($Stage.UserCommands) {
                 $CmdCount = 1
-                ForEach ($UserCommand in $Commands) {
-                    New-ProcessLog -logSev i -logStage $Stage -logStep 'Run User Command' -logExField1 "Node: $($Node.hostname)" -logExField2 "Command Number: $($CmdCount)" -logMessage "Command: $($UserCommand)"
-                    Try {
-                        $HostResult = Invoke-Command -Session $NodeSession -ScriptBlock {bash -c $UserCommand} -ErrorAction SilentlyContinue
-                    } Catch {
-                        write-host $_
+                $NodeSession = Test-LrClusterRemoteAccess -Hostnames $($Node.ipaddr) -Path $SSHKeyPath
+                ForEach ($UserCommand in $Stage.UserCommands) {
+                    switch ($Usercommand.Type) {
+                        'bash' {
+                            New-ProcessLog -logSev i -logStage $($Stage.Name) -logStep 'Run User Command' -logExField1 "Node: $($Node.hostname)" -logExField2 "Type: Bash" -logMessage "Command: $($UserCommand.Command)"
+                            Try {
+                                $HostResult = Invoke-Command -Session $NodeSession -ScriptBlock {& $args[0] $args[1] $args[2]} -ArgumentList 'bash', '-c', $UserCommand.Command -ErrorAction SilentlyContinue
+                            } Catch {
+                                write-host $_
+                            }
+                        }
+                        'pwsh' {
+                            New-ProcessLog -logSev i -logStage $($Stage.Name) -logStep 'Run User Command' -logExField1 "Node: $($Node.hostname)" -logExField2 "Type: Pwsh" -logMessage "Command: $($UserCommand.Command)"
+                            try {
+                                $HostResult = Invoke-Command -Session $NodeSession -ScriptBlock {& $args[0]} -ArgumentList $UserCommand.Command -ErrorAction SilentlyContinue
+                            } Catch {
+                                write-host $_
+                            }
+                        }
+                        default {
+                            New-ProcessLog -logSev e -logStage $($Stage.Name) -logStep 'Run User Command' -logExField1 "Node: $($Node.hostname)" -logExField2 "Type: Error" -logMessage "Command type error.  Submitted type: $($UserCommand.Type)"
+                        }
                     }
                     $CmdCount += 1
                 }
-                New-ProcessLog -logSev i -logStage $Stage -logStep 'Run User Command' -logExField2 "Node: $($Node.hostname)" -logMessage "End Step"
+            } else {
+                New-ProcessLog -logSev i -logStage $($Stage.Name) -logStep 'Run User Command' -logExField1 "Node: $($Node.hostname)" -logMessage "No user commands defined."
             }
         }
+        New-ProcessLog -logSev i -logStage $($Stage.Name) -logStep 'Run User Command' -logMessage "End Step"
     }
 }
